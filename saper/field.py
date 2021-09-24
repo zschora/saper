a603d0e86b8cac721330cd35c81a80752fc96c74
zschora <yegorssh@gmail.com>
2021-09-25 02:48:55 +0300

    +win with all true flags

--- saper/field.py
+++ saper/field.py
@@ -6,6 +6,7 @@ class field:
         self._field = [[0 for _ in range(n)] for _ in range(n)]
         self._game = [[0 for _ in range(n)] for _ in range(n)]
         self._not_open = n * n - bombs_number
+        self._not_flagged = bombs_number
         self._losing = False
         
         for i in range(bombs_number):
@@ -29,19 +30,25 @@ class field:
 
     def check(self, i1, i2):
         current = self._field[i1][i2]
-        self._game[i1][i2] = 1
         if current == -1:
             self._losing = True
         else:
+            self._not_flagged -= 1 if self._game[i1][i2] == 2 else 0
             self._not_open -= 1
+        self._game[i1][i2] = 1
 
     def get_bombs_num(self, i1, i2):
         return self._field[i1][i2]
 
     def set_flag(self, i1, i2):
         self._game[i1][i2] = 2
+        if self._field[i1][i2] == -1:
+            self._not_flagged -= 1
+        else:
+            self._not_flagged += 1
 
     def print(self):
+        print(self._not_flagged)
         for i1 in range(len(self._field)):
             for i2 in range(len(self._field)):
                 if self._game[i1][i2] == 0:
@@ -55,7 +62,7 @@ class field:
     def check_win(self):
         if self._losing:
             return 'lose'
-        elif self._not_open == 0:
+        elif self._not_open == 0 or self._not_flagged == 0:
             return 'win'
         else:
             return 'not the end'
